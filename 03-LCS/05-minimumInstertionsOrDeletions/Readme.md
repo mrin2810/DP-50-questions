8933734efcde15e4fc43f05ecd23ae45d81545ab
Mrinmayee Rane <mrinmayeerane2810@gmail.com>
2024-10-27 12:33:32 -0400

    Added code for Leetcode

--- 03-LCS/05-minimumInstertionsOrDeletions/Readme.md
+++ 03-LCS/05-minimumInstertionsOrDeletions/Readme.md
@@ -4,6 +4,8 @@
 Given two strings `str1` and `str2`. The task is to remove or insert the minimum number of characters from/in `str1` so as to transform it into `str2`. 
 It could be possible that the same character needs to be removed/deleted from one point of `str1` and inserted to some another point.
 
+[Leetcode](https://leetcode.com/problems/delete-operation-for-two-strings/description/)
+
 ### Example 1:
 ```text
 Input: str1 = "heap", str2 = "pea"
--- 03-LCS/05-minimumInstertionsOrDeletions/src/MinNoOFInsertions.java
+++ 03-LCS/05-minimumInstertionsOrDeletions/src/MinNoOFInsertions.java
@@ -1,7 +1,7 @@
 public class MinNoOFInsertions {
-    static String lcs(String s1, String s2) {
-        int m = s1.length();
-        int n = s2.length();
+    static String lcs(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
         int[][] dp = new int[m + 1][n + 1];
         // Initialize
         for (int i = 1; i <= m; i++) {
@@ -15,7 +15,7 @@ public class MinNoOFInsertions {
 
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
-                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
                     dp[i][j] = dp[i - 1][j - 1] + 1;
                 } else {
                     dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
