b86edce7887cd4b2df2d31646b3217d0ecdec704
Mrinmayee Rane <mrinmayeerane2810@gmail.com>
2024-11-02 14:30:33 -0400

    Longest Repeating Subsequence

--- 03-LCS/06-longestRepeatingSubsequence/Readme.md
+++ 03-LCS/06-longestRepeatingSubsequence/Readme.md
@@ -2,16 +2,28 @@
 
 ## [Problem Statement](https://www.geeksforgeeks.org/longest-repeated-subsequence/)
 
+Given a string, find the length of the longest repeating subsequence, such that the two subsequences don’t have same string character at the same position, i.e. any ith character in the two subsequences shouldn’t have the same index in the original string.
+
 [geeksforgeeks](https://www.geeksforgeeks.org/longest-repeated-subsequence/)
 
 [Leetcode](https://leetcode.com/discuss/interview-question/3635895/Longest-Repeating-Subsequence)
 
 ## Is this LCS?
 
+Question is talking about subsequence so, we will be thinking in LCS direction.
+
 ### How it is related to LCS?
 
-## Variation
+Let's think how would we find a repeating subsequence?
+- We would need a LCS of this string with itself, which has repeating characters.
+- In other words LCS where i != j but s[i] == s[j]
+- So, let's see example:
+    - ![img.png](img.png)
 
-### Code Variation
+## Variation
+- In the code we just want to add this one line
+  - ``` if (a[i - 1] == b[j - 1] && i != j)```
+- This variation would make the difference and we would get the LCS.
 
 ## Completed Code
+link
--- 03-LCS/06-longestRepeatingSubsequence/src/LRSDP.java
+++ 03-LCS/06-longestRepeatingSubsequence/src/LRSDP.java
@@ -1,12 +1,41 @@
 public class LRSDP {
-    static String longestRepeatedSubSeq(String str)
+    static int longestRepeatedSubSeq(String str)
     {
-       return "";
+       String a = new String(str);
+       String b = new String(str);
+
+       int m = a.length();
+       int n = b.length();
+
+       int[][] dp = new int[m + 1][n + 1];
+
+       // Initialize
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                } else {
+                    dp[i][j] = -1;
+                }
+            }
+        }
+
+        // LCS Code
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (a.charAt(i - 1) == b.charAt(j - 1) && i != j) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[m][n];
     }
 
     public static void main(String args[])
     {
         String str = "AABEBCDD";
-        System.out.println(longestRepeatedSubSeq(str));
+        System.out.println(longestRepeatedSubSeq(str)); // Output: 3 (ABD)
     }
 }
